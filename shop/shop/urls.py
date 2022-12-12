7f4067a42de1e1ca68e6ad8a030be52cdd319e24
Adam <adam.tantas2@mail.dcu.ie>
2022-12-12 21:01:53 +0000

    project

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -18,10 +18,15 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+import django.views.static import serve
+import django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
